855c11b1d08a7d48681b2250e49dbaefc0527dde
Loonylap <60702848+Loonylap@users.noreply.github.com>
2020-05-06 01:39:44 +0300

    popup window

--- js/src/hamburger.js
+++ js/src/hamburger.js
@@ -1,22 +1,20 @@
-$(".hamburger").click(function(){
+let fun = function() {
     $(".header nav").toggleClass("hamburger-menu")
     $(".header nav").toggleClass("menu")
     $("body").toggleClass("inactive")
-    
-})
+}
+
+$(".hamburger").click(fun);
 
 $(".menu__link").click(function(){
     if ($(".header nav").hasClass('hamburger-menu')) {
-        $(".header nav").toggleClass("hamburger-menu")
-        $(".header nav").toggleClass("menu")
-        $("body").toggleClass("inactive")
+        fun();
     }
 })
 
 $(document).on('click', '.hamburger-menu', function (e) {
     if(e.offsetX<0) {
-        $(".header nav").toggleClass("hamburger-menu")
-        $(".header nav").toggleClass("menu")
-        $("body").toggleClass("inactive")
+        fun();
     }
-  })
+})
+
---
+++ js/src/query.js
@@ -0,0 +1,39 @@
+let fun1 = function() {
+    $("#3").toggleClass("purchase-window__active")
+    $("#3").toggleClass("purchase-window")
+    $("body").toggleClass("inactive")
+    $('.success_msg').hide()
+    $('.fail_msg').hide()
+}
+
+
+
+$("#2").click(fun1);
+
+$('#1').click(function () {
+    let fn = $(".name").val();
+    let ln = $(".phone").val();
+    $.ajax({
+        url: "https://httpbin.org/status/200",
+        success: function () {
+            $('.success_msg').toggle()
+        },
+        error: function () {
+            $('.fail_msg').toggle()
+        }
+    })
+});
+
+
+jQuery(function($){
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $("#3"); // тут указываем ID элемента
+        
+		if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0
+            && div.hasClass('purchase-window__active'))
+            { // и не по его дочерним элементам
+			fun1()
+		}
+	});
+});
